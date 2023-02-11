22e47cda10a932126940026afefb4f0f9b3f0b4d
Daniel Alejandro Gaona Mercado <daniel.gaona@pjedomex.gob.mx>
2023-02-11 10:56:51 -0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { app, ref, getDatabase, set } from './config/firebase';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
+
+  const [user, setUser] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [confirmEmail, setConfirmEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirPassword, setConfirmPassword] = useState();
+
+
+  function writeUserData(userId, user, email, firstName,lastName, password) {
+    const dbCurso = getDatabase();
+    set(ref(dbCurso, 'users/' + userId), {
+      username: user,
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: password,
+    });
+  }
+
+  const prueba = () => {
+    //console.log(email, nombre);
+    //if(email ===confirmemail){
+    let myuuid = uuidv4();
+    console.log(myuuid);
+    writeUserData(myuuid, user, email, firstName,lastName,password);
+    // }
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.form}>
+      <View >
+        <Text>User:</Text>
+        <TextInput style={{borderStyle:'1'}} placeholder='' onChangeText={setUser} />
+      </View>
+      <View>
+        <Text>First Name:</Text>
+        <TextInput placeholder='' onChangeText={setFirstName} />
+      </View>
+      <View>
+        <Text>Last Name:</Text>
+        <TextInput placeholder='' onChangeText={setLastName} />
+      </View>
+      <View>
+        <Text>Email:</Text>
+        <TextInput placeholder='' onChangeText={setEmail} />
+      </View>
+      <View>
+        <Text>Confirma Email:</Text>
+        <TextInput placeholder='' onChangeText={setConfirmEmail} />
+      </View>
+      <View>
+        <Text>Password:</Text>
+        <TextInput placeholder='' onChangeText={setPassword} />
+      </View>
+      <View>
+        <Text>Confirm Password:</Text>
+        <TextInput placeholder='' onChangeText={setConfirmPassword} />
+      </View>
+      </View>
+      <View style={styles.ButtonContainer}>
+        <Button onPress={prueba} title="Send" />
+        <Button onPress={prueba} title="Limpiar" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +79,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ButtonContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    padding: '10px',
+  },
+  form:{
+    flex:6,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+
+  }
 });
